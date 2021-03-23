30307fbaf2a38b41e63b3d85ea7fa3b06931dc80
analivia1709 <tresliviaana@gmail.com>
2021-03-23 16:00:20 -0300

    Componentes entrar e cadastrar

---
+++ Projeto/blogPessoal/src/app/cadastar/cadastar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastar.component.html',
+  styleUrls: ['./cadastar.component.css']
+})
+export class CadastarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
