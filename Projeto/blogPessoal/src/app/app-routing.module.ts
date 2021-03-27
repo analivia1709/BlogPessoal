7b131d8f1b2d45dee70f940af67429ea0eb747d2
analivia1709 <tresliviaana@gmail.com>
2021-03-27 00:40:26 -0300

    Consumo de AP - Login e Cadastro

--- Projeto/blogPessoal/src/app/app-routing.module.ts
+++ Projeto/blogPessoal/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import{InicioComponent} from './inicio/inicio.component'
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastarComponent } from './cadastar/cadastar.component';
@@ -6,10 +7,11 @@ import { EntrarComponent } from './entrar/entrar.component';
 
 const routes: Routes = [
 
-  {path:'',redirectTo : 'entrar' ,  pathMatch :'full'},
+  { path: '', redirectTo: 'entrar', pathMatch: 'full' },
 
-  {path:'entrar',component:EntrarComponent },
-    {path:'cadastrar', component: CadastarComponent}
+  { path: 'entrar', component: EntrarComponent },
+  { path: 'cadastrar', component: CadastarComponent },
+  { path: 'inicio ', component: InicioComponent }
 
 
 ];
--- Projeto/blogPessoal/src/app/model/UsuarioLogin.ts
+++ Projeto/blogPessoal/src/app/model/UsuarioLogin.ts
@@ -6,4 +6,5 @@ export class UsuarioLogin {
     public token: string
     public Foto: string
     public tipo: string
+    public idUsuario: number 
 }
